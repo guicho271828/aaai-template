7caada13b245fa565c14d89b49ac25b9627373c9
Masataro Asai <guicho2.71828@gmail.com>
2023-06-14 15:13:10 -0400

    combine main + supplement as one archive

--- scripts/submission.mk
+++ scripts/submission.mk
@@ -94,17 +94,25 @@ clean-submission:
 	-rm -rf *.submission *.tar.gz *.zip
 
 
-archive: $(name).submission
+archive: $(name).submission supplemental.submission
 	@echo "submission.mk: creating a camera-ready submission archive (tar.gz, zip) WITHOUT style files (use 'make arxiv' instead to include them)"
-	-rm $</*.sty $</*.bst
+	-rm *.submission/*.sty *.submission/*.bst
 	-rm $(name).tar.gz
-	cd $< ; tar cvzf ../$(name).tar.gz *
+	cd $(name).submission ; tar cvf ../$(name).tar *
+	cd supplemental.submission ; tar rvf ../$(name).tar *
+	gzip $(name).tar
 	-rm $(name).zip
-	cd $< ; zip -r ../$(name).zip *
+	cd $(name).submission ; zip -r ../$(name).zip *
+	cd supplemental.submission ; zip -r ../$(name).zip *
 
 
-arxiv: $(name).submission
+arxiv: $(name).submission supplemental.submission
 	@echo "submission.mk: creating an Arxiv submission (tar.gz, zip) WITH style files (use 'make archive' instead to exclude them)"
-	-rm $(name).tar.gz $(name).zip
-	cd $< ; tar cvzf ../$(name).tar.gz *
-	cd $< ; zip -r ../$(name).zip *
+	-rm $(name).tar.gz
+	cd $(name).submission ; tar cvf ../$(name).tar *
+	cd supplemental.submission ; tar rvf ../$(name).tar *
+	gzip $(name).tar
+	-rm $(name).zip
+	cd $(name).submission ; zip -r ../$(name).zip *
+	cd supplemental.submission ; zip -r ../$(name).zip *
+
